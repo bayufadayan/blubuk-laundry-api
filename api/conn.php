b50c82b2bc6189d4698d39471d0633cec0cfde98
bayufadayan <bayufadayan@gmail.com>
2025-02-10 16:58:41 +0700

    modify variable name

--- api/conn.php
+++ api/conn.php
@@ -1,6 +1,6 @@
 <?php
 $host = getenv('DB_HOST');
-$username = getenv('DB_USER');
+$user = getenv('DB_USER');
 $password = getenv('DB_PASS');
 $dbname = getenv('DB_NAME');
 $port = getenv('DB_PORT');
